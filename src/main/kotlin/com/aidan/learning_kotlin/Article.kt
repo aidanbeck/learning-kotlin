e508849196e64f417cc6413613dd5d4e1962011f
aidanbeck <aidanmbeck@outlook.com>
2025-11-30 19:34:58 -0600

    basic kotlin data class & value types

---
+++ src/main/kotlin/com/aidan/learning_kotlin/Article.kt
@@ -0,0 +1,14 @@
+package com.aidan.learning_kotlin
+
+import java.time.LocalDateTime
+
+data class Article(
+    val title: String, // constant
+    val content: String?, // nullable constant
+    val createdAt: LocalDateTime = LocalDateTime.now(), // default value
+    var views: Integer // variable
+
+    // a "slug" is the directory of the content under the website domain
+    // https://blog/my-first-title  My First Title
+    //val slug = title.toSlug()
+)
