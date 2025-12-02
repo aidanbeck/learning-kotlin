0224076cbef0390241ffd88708e9275cd0486094
aidanbeck <aidanmbeck@outlook.com>
2025-12-02 00:32:50 -0600

    conclusion and to-do

--- src/main/kotlin/com/aidan/learning_kotlin/LearningKotlinApplication.kt
+++ src/main/kotlin/com/aidan/learning_kotlin/LearningKotlinApplication.kt
@@ -17,4 +17,14 @@ fun main(args: Array<String>) {
 	I will work ahead of the tutorial to give my best attempt at accomplishing the task,
 	then go back and compare their work to my own, taking note of observations, corrections,
 	and questions I have.
+
+	Conclusion: I still do not prefer video tutorials. I find it more disorienting than documentation,
+	where explanations for any kind of phenomenon is often linked to or described with accurate and
+	searchable vocabulary. I also question what is and isn't best practice, and whether I should trust
+	the credentials of the video I am watching. Still, I'm glad I tried this out.
+
+	I'm going to sleep now, but will leave this TODO for myself.
+	- Adjust comment notes throughout to accurately describe the new JPA implementations, instead of the old dummy data manipulation.
+	- Answer all questions marked with STUDY
+	- Condense notes down into my own personal notes collection.
  */
